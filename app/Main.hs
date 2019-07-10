d427115116d3684f7d6b8c4bbb0403d38a7fefec
antonholmberg <anton.holmberg@protonmail.com>
2019-07-10 09:05:05 +0200

    Add arguments parser

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,23 @@ module Main where
 import           Api                      (buildsForPage, fetch)
 import           Control.Monad.Reader     (runReaderT)
 import           Control.Monad.State.Lazy (runStateT)
+import           System.Console.ArgParser (CmdLnInterface, mkDefaultApp,
+                                           mkSubParser, optPos, parsedBy,
+                                           runApp)
+
+data Command =
+  GetBuilds Int
+  deriving (Eq, Show)
+
+parser :: IO (CmdLnInterface Command)
+parser =
+  mkSubParser
+    [("builds", mkDefaultApp (GetBuilds `parsedBy` optPos 0 "page") "builds")]
+
+runCommand :: Command -> IO ()
+runCommand (GetBuilds i) = print =<< builds i
+  where
+    builds = fetch . buildsForPage
 
 main :: IO ()
-main = do
-  page <- fetch $ buildsForPage 0
-  print page
+main = parser >>= flip runApp runCommand
